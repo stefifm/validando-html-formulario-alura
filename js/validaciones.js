639a94c961edd55b5c68f1eeeab07e653d0abb18
stefifm <bruerastefania@gmail.com>
2023-02-21 14:17:22 -0300

    Add message errors

--- js/validaciones.js
+++ js/validaciones.js
@@ -3,10 +3,54 @@ export function valida (input) {
   if (validadores[tipoInput]) {
     validadores[tipoInput](input)
   }
+
+  if (input.validity.valid) {
+    input.parentElement.classList.remove('input-container--invalid')
+    input.parentElement.querySelector('.input-message-error').innerHTML = ''
+  } else {
+    input.parentElement.classList.add('input-container--invalid')
+    input.parentElement.querySelector('.input-message-error').innerHTML = mostrarError(tipoInput, input)
+  }
 }
+
+const mensajeError = {
+  nombre: {
+    valueMissing: 'El campo nombre no puede estar vacío'
+  },
+  email: {
+    valueMissing: 'El campo email no puede estar vacío',
+    typeMismatch: 'El correo no es válido'
+  },
+  password: {
+    valueMissing: 'El campo contraseña no puede estar vacío',
+    patternMismatch: 'Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra en mayúscula, un número y no puede contener caracteres especiales'
+  },
+  nacimiento: {
+    valueMissing: 'El campo fecha de nacimiento no puede estar vacío',
+    customError: 'Debes tener 18 años o más'
+  }
+}
+
+const tipoErrores = [
+  'valueMissing',
+  'typeMismatch',
+  'patternMismatch',
+  'customError'
+]
 
 const validadores = {
   nacimiento: (input) => validarNacimiento(input)
+}
+
+function mostrarError (tipoInput, input) {
+  let mensaje = ''
+  tipoErrores.forEach(error => {
+    if (input.validity[error]) {
+      mensaje = mensajeError[tipoInput][error]
+    }
+  })
+
+  return mensaje
 }
 
 function validarNacimiento (input) {
